e64138a5a8bec0cf7622245b2911367f8a5d13c6
Luiz Gustavo Zanoni <luizzanoni47@gmail.com>
2024-06-04 20:45:43 -0300

    config banco1

--- index.php
+++ index.php
@@ -3,3 +3,8 @@
 $pdo = new PDO("mysql:dbname=test;host=127.0.0.1:3306", "root", "");
 
 $sql = $pdo->query('SELECT * FROM usuario');
+
+$dados = $sql->fetchAll(pdo::FETCH_ASSOC);
+
+echo '<pre>';
+print_r($dados);
